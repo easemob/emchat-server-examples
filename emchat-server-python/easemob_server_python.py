e2a2700d9a62d52dd5c51444333480d71dbacf8d
Strong Liu <stliu@hibernate.org>
2014-07-08 12:39:12 +0800

    update python upload file example

--- emchat-server-python/easemob_server_python.py
+++ emchat-server-python/easemob_server_python.py
@@ -10,6 +10,7 @@ import string
 import random
 
 JSON_HEADER = {'content-type': 'application/json'}
+# EASEMOB_HOST = "http://localhost:8080"#
 EASEMOB_HOST = "https://a1.easemob.com"
 
 DEBUG = False
@@ -185,8 +186,20 @@ def delete_user(org, app, auth, username):
     DELETE /{org}/{app}/users/{username}
     """
     url = EASEMOB_HOST+("/%s/%s/users/%s" % (org, app, username))
-    return post(url, payload, auth)    
-    
+    return post(url, {}, auth)
+
+def send_file(org, app, auth, file_path):
+    """上传文件
+    上传文件
+    curl --verbose --header "Authorization: Bearer YWMtz1hFWOZpEeOPpcmw1FB0RwAAAUZnAv0D7y9-i4c9_c4rcx1qJDduwylRe7Y" \
+    --header "restrict-access:true" --form file=@/Users/stliu/a.jpg \
+    http://a1.easemob.com/easemob-demo/chatdemoui/chatfiles
+    """
+    url = EASEMOB_HOST+("/%s/%s/chatfiles" % (org, app))
+    # files = {'file': open(file_path, 'rb')}
+    files = {'file': ('report.xls', open(file_path, 'rb'), 'image/jpeg', {'Expires': '0'})}
+    r = requests.post(url, files=files,  auth=auth)
+    return http_result(r)
  
 
 if __name__ == '__main__':
@@ -202,7 +215,7 @@ if __name__ == '__main__':
     app_admin_username = s['app']['admin']['username']
     app_admin_password = s['app']['admin']['password']
     # org_admin的认证方式
-    org_admin_auth = OrgAdminAccountAuth(org_admin_username,org_admin_password)
+    org_admin_auth = OrgAdminAccountAuth(org_admin_username, org_admin_password)
     # 获取org管理员的token
     print "Get org admin token: " + org_admin_auth.get_token()
     # 获取app管理员的token
@@ -210,27 +223,30 @@ if __name__ == '__main__':
     print "Get app admin token:" + app_admin_auth.get_token()
     # 通过client id和secret来获取app管理员的token
     app_client_auth = AppClientAuth(org, app, client_id, client_secret)
-    print "Get app admin tokey with client id/secret: " + app_client_auth.get_token()
+    print "Get app admin token with client id/secret: " + app_client_auth.get_token()
     
     print "now let's register some users...."
-    app_users = []
-    for i in range(10):
-        username = id_generator()
-        
-        password = '123456'
-        success, result = register_new_user(org, app, app_admin_auth, username, password)
-        if success:
-            print "registered new user %s in appkey[%s]" % (username, appkey)
-            app_users.append(username)
-        else:
-            print "failed to register new user %s in appkey[%s]" % (username, appkey)
-            
-    print "now let's delete users just created, this time we're using app_client_auth"
-    
-    for username in app_users:
-        success, result = delete_user(org, app,app_client_auth, username)
-        if success:
-            print "user %s is deleted"
-        else:
-            print "failed to delete user %s"
-    
+    # app_users = []
+   #  for i in range(10):
+   #      username = id_generator()
+   #
+   #      password = '123456'
+   #      success, result = register_new_user(org, app, app_admin_auth, username, password)
+   #      if success:
+   #          print "registered new user %s in appkey[%s]" % (username, appkey)
+   #          app_users.append(username)
+   #      else:
+   #          print "failed to register new user %s in appkey[%s]" % (username, appkey)
+   #
+   #  print "now let's delete users just created, this time we're using app_client_auth"
+   #
+   #  for username in app_users:
+   #      success, result = delete_user(org, app,app_client_auth, username)
+   #      if success:
+   #          print "user %s is deleted"
+   #      else:
+   #          print "failed to delete user %s"
+    success, result = send_file(org, app, app_client_auth, '../zjg.jpg')
+    print result
+   
+
