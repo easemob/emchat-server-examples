9bfbd0e4bc50a64f74818dbfcf9d2819ebd04129
Lynch <sunshinewei_lee@126.com>
2014-07-08 18:52:54 +0800

    add RestAPI Demo: get

---
+++ emchat-server-java/src/main/java/com/easemob/server/example/jax/rs/EasemobRESTSSLAPI.java
@@ -0,0 +1,150 @@
+package com.easemob.server.example.jax.rs;
+
+import net.sf.json.JSONObject;
+
+import org.apache.log4j.Logger;
+
+/**
+ * EasemobRESTAPI Demo
+ * 
+ * @author Lynch
+ * 
+ */
+public class EasemobRESTSSLAPI {
+
+	private static Logger logger = Logger.getLogger(HttpUtils.class);
+
+	private static HttpsUtils httpsUtils = new HttpsUtils();
+
+	/**
+	 * 创建用户
+	 * 
+	 * @param host
+	 *            IP或者域名
+	 * @param port
+	 *            端口
+	 * @param appKey
+	 *            easemob-demo#chatdemo
+	 * @param postBody
+	 *            封装了用户属性的json对象
+	 * @param token
+	 *            admin级别token
+	 * @return
+	 */
+	public static JSONObject createNewUser(String host, String appKey, JSONObject body, String token) {
+
+		String orgName = appKey.substring(0, appKey.lastIndexOf("#"));
+		String appName = appKey.substring(appKey.lastIndexOf("#") + 1);
+
+		String rest = orgName + "/" + appName + "/users";
+
+		logger.error("url:" + host + rest);
+		String reqURL = "https://" + host + "/" + rest;
+		String result = httpsUtils.sendSSLRequest(reqURL, token, body.toString(),
+				httpsUtils.Method_POST);
+
+		return JSONObject.fromObject(result);
+	}
+
+	/**
+	 * 删除用户
+	 * 
+	 * @param host
+	 *            IP或者域名
+	 * @param port
+	 *            端口
+	 * @param appKey
+	 *            easemob-demo#chatdemo
+	 * @param id
+	 *            usename or uuid
+	 * @param token
+	 *            admin级别token
+	 * @return
+	 */
+	public static JSONObject deleteUser(String host, String appKey, String id, String token) {
+
+		String orgName = appKey.substring(0, appKey.lastIndexOf("#"));
+		String appName = appKey.substring(appKey.lastIndexOf("#") + 1);
+
+		String rest = orgName + "/" + appName + "/users/" + id;
+
+		logger.error("url:" + host + rest);
+		String reqURL = "https://" + host + "/" + rest;
+		String result = httpsUtils.sendSSLRequest(reqURL, token, null, httpsUtils.Method_DELETE);
+		return JSONObject.fromObject(result);
+	}
+
+	/**
+	 * 获取token
+	 * 
+	 * @param host
+	 *            IP或者域名
+	 * @param port
+	 *            端口
+	 * @param appKey
+	 *            easemob-demo#chatdemo
+	 * @param isAdmin
+	 *            org管理员token true, IM用户token false
+	 * @param postBody
+	 *            POST请求体
+	 * @return
+	 */
+	public static String getAccessToken(String host, String appKey, Boolean isAdmin,
+			JSONObject postBody) {
+		String accessToken = "";
+		String orgName = appKey.substring(0, appKey.lastIndexOf("#"));
+		String appName = appKey.substring(appKey.lastIndexOf("#") + 1);
+
+		String rest = "management/token";
+		if (!isAdmin) {
+			rest = orgName + "/" + appName + "/token";
+		}
+
+		logger.error("url: " + "https://" + host + "/" + rest);
+		String reqURL = "https://" + host + "/" + rest;
+		String result = httpsUtils.sendSSLRequest(reqURL, null, postBody.toString(),
+				httpsUtils.Method_POST);
+		JSONObject jsonObject = JSONObject.fromObject(result);
+
+		if (null == jsonObject.get("error")) {
+			accessToken = (String) jsonObject.get("access_token");
+		}
+
+		return accessToken;
+	}
+
+	public static void main(String[] args) {
+		String host = "a1.easemob.com";
+		String appKey = "easemob-playground#test1";
+
+		// 获取IM用户token
+		JSONObject getIMAccessTokenPostBody = new JSONObject();
+		getIMAccessTokenPostBody.put("grant_type", "password");
+		getIMAccessTokenPostBody.put("username", "testuser2");
+		getIMAccessTokenPostBody.put("password", "testuser1");
+		// String imToken = EasemobRESTSSLAPI.getAccessToken(host, appKey, false,
+		// getIMAccessTokenPostBody);
+		// System.out.println(imToken);
+
+		// 获取管理员token
+		JSONObject getAccessTokenPostBody = new JSONObject();
+		getAccessTokenPostBody.put("grant_type", "password");
+		getAccessTokenPostBody.put("username", "zhangjianguo");
+		getAccessTokenPostBody.put("password", "zhangjianguo");
+		// String adminToken = EasemobRESTSSLAPI.getAccessToken(host, appKey, true,
+		// getAccessTokenPostBody);
+		// System.out.println(adminToken);
+
+		// 创建用户
+		JSONObject createNewUserPostBody = new JSONObject();
+		createNewUserPostBody.put("username", "testuser2");
+		createNewUserPostBody.put("password", "testuser2");
+		createNewUserPostBody.put("addr", "BJFS");
+		String adminToken = "YWMt1dg3rAaLEeSeLyOsOrWKZAAAAUc5mRF9trZgseW8nbEiRbTuEzm6sqlzizc";
+		// EasemobRESTSSLAPI.createNewUser(host, appKey, createNewUserPostBody, adminToken);
+
+		// 删除用户
+		String id = "testuser2";
+		EasemobRESTSSLAPI.deleteUser(host, appKey, id, adminToken);
+	}
+}
---
+++ emchat-server-java/src/main/java/com/easemob/server/example/jax/rs/HttpsUtils.java
@@ -0,0 +1,117 @@
+package com.easemob.server.example.jax.rs;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+
+/**
+ * HttpClient Utils
+ * 
+ * @author Lynch
+ * 
+ */
+@SuppressWarnings("all")
+public class HttpsUtils {
+
+	public static String Method_GET = "GET";
+	public static String Method_POST = "POST";
+	public static String Method_PUT = "PUT";
+	public static String Method_DELETE = "DELETE";
+
+	private static Logger logger = Logger.getLogger(HttpsUtils.class);
+
+	/**
+	 * Send SSL Request
+	 * 
+	 * @param reqURL
+	 * @param token
+	 * @param str
+	 * @return
+	 */
+	@SuppressWarnings("finally")
+	public String sendSSLRequest(String reqURL, String token, String body, String method) {
+
+		String responseContent = null;
+		HttpClient httpClient = new DefaultHttpClient();
+		X509TrustManager xtm = new X509TrustManager() {
+			public void checkClientTrusted(X509Certificate[] chain, String authType)
+					throws CertificateException {
+			}
+
+			public void checkServerTrusted(X509Certificate[] chain, String authType)
+					throws CertificateException {
+			}
+
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+		};
+
+		try {
+			SSLContext ctx = SSLContext.getInstance("TLS");
+
+			ctx.init(null, new TrustManager[] { xtm }, null);
+			SSLSocketFactory socketFactory = new SSLSocketFactory(ctx);
+			httpClient.getConnectionManager().getSchemeRegistry()
+					.register(new Scheme("https", 443, socketFactory));
+			HttpResponse response = null;
+
+			if (method.equals(this.Method_POST)) {
+				HttpPost httpPost = new HttpPost(reqURL);
+				httpPost.setEntity(new StringEntity(body, "UTF-8"));
+				if (token != null) {
+					httpPost.setHeader("Authorization", "Bearer " + token);
+				}
+				response = httpClient.execute(httpPost);
+			} else if (method.equals(this.Method_PUT)) {
+				HttpPut httpPut = new HttpPut(reqURL);
+				httpPut.setEntity(new StringEntity(body, "UTF-8"));
+				if (token != null) {
+					httpPut.setHeader("Authorization", "Bearer " + token);
+				}
+				response = httpClient.execute(httpPut);
+			} else if (method.equals(this.Method_GET)) {
+				HttpGet httpGet = new HttpGet(reqURL);
+				if (token != null) {
+					httpGet.setHeader("Authorization", "Bearer " + token);
+				}
+				response = httpClient.execute(httpGet);
+			} else if (method.equals(this.Method_DELETE)) {
+				HttpDelete httpDelete = new HttpDelete(reqURL);
+				if (token != null) {
+					httpDelete.setHeader("Authorization", "Bearer " + token);
+				}
+				response = httpClient.execute(httpDelete);
+			}
+
+			HttpEntity entity = response.getEntity();
+
+			if (null != entity) {
+				responseContent = EntityUtils.toString(entity, "UTF-8");
+				EntityUtils.consume(entity);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException();
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+			return responseContent;
+		}
+	}
+}
