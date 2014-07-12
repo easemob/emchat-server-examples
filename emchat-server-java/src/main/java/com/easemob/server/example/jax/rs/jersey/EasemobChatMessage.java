b56e23426e89b88ccb31fddf9d50fff27d89065e
Lynch <sunshinewei_lee@126.com>
2014-07-12 18:08:15 +0800

    add jersey demo

---
+++ emchat-server-java/src/main/java/com/easemob/server/example/jax/rs/jersey/EasemobChatMessage.java
@@ -0,0 +1,17 @@
+package com.easemob.server.example.jax.rs.jersey;
+
+/**
+ * REST API Demo : 聊天消息导出REST API Jersey2.9实现
+ * 
+ * Doc URL: http://developer.easemob.com/docs/emchat/rest/chatmessage.html
+ * 
+ * @author Lynch 2014-07-12
+ * 
+ */
+public class EasemobChatMessage {
+
+	// 图片语音文件上传、下载
+	public void mediaTransform() {
+
+	}
+}
