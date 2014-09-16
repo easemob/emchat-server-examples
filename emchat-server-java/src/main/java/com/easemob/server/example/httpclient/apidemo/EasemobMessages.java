39ee6cefc356919bd0a7f312f34b1500565c1a04
Lynch <sunshinewei_lee@126.com>
2014-09-16 14:55:15 +0800

    sendmessages api ok

--- emchat-server-java/src/main/java/com/easemob/server/example/httpclient/apidemo/EasemobMessages.java
+++ emchat-server-java/src/main/java/com/easemob/server/example/httpclient/apidemo/EasemobMessages.java
@@ -1,10 +1,160 @@
 package com.easemob.server.example.httpclient.apidemo;
 
+import java.net.URL;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.easemob.server.example.comm.Constants;
+import com.easemob.server.example.comm.HTTPMethod;
+import com.easemob.server.example.comm.Roles;
+import com.easemob.server.example.httpclient.utils.HTTPClientUtils;
+import com.easemob.server.example.httpclient.vo.Credentail;
+import com.easemob.server.example.httpclient.vo.EndPoints;
+import com.easemob.server.example.httpclient.vo.UsernamePasswordCredentail;
+import com.easemob.server.example.jersey.utils.JerseyUtils;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 /**
+ * REST API Demo: 发送消息 REST API HttpClient4.3实现
+ * 
+ * Doc URL: http://www.easemob.com/docs/rest/sendmessage/
  * 
  * @author Lynch 2014-09-15
  *
  */
 public class EasemobMessages {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(EasemobMessages.class);
+
+	private static final String APPKEY = Constants.APPKEY;
+
+	private static JsonNodeFactory factory = new JsonNodeFactory(false);
+
+	/**
+	 * 检测用户是否在线
+	 * 
+	 * @param token
+	 * @param user
+	 * @return
+	 */
+	public static ObjectNode getUserStatus(String targetUserPrimaryKey) {
+
+		ObjectNode objectNode = factory.objectNode();
+
+		// check appKey format
+		if (!JerseyUtils.match("[0-9a-zA-Z]+#[0-9a-zA-Z]+", APPKEY)) {
+			LOGGER.error("Bad format of Appkey: " + APPKEY);
+
+			objectNode.put("message", "Bad format of Appkey");
+
+			return objectNode;
+		}
+
+		// check properties that must be provided
+		if (StringUtils.isEmpty(targetUserPrimaryKey)) {
+			LOGGER.error("You must provided a targetUserPrimaryKey .");
+
+			objectNode.put("message", "You must provided a targetUserPrimaryKey .");
+
+			return objectNode;
+		}
+
+		try {
+
+			Credentail credentail = new UsernamePasswordCredentail(Constants.APP_ADMIN_USERNAME,
+					Constants.APP_ADMIN_PASSWORD, Roles.USER_ROLE_APPADMIN);
+
+			URL userStatusUrl = HTTPClientUtils.getURL(Constants.APPKEY.replace("#", "/") + "/users/"
+					+ targetUserPrimaryKey + "/status");
+
+			objectNode = HTTPClientUtils.sendHTTPRequest(userStatusUrl, null, credentail, HTTPMethod.METHOD_GET);
+
+			String userStatus = objectNode.get("data").path(targetUserPrimaryKey).asText();
+			if ("online".equals(userStatus)) {
+				LOGGER.error(String.format("The status of user[%s] is : [%s] .", targetUserPrimaryKey, userStatus));
+			} else if ("offline".equals(userStatus)) {
+				LOGGER.error(String.format("The status of user[%s] is : [%s] .", targetUserPrimaryKey, userStatus));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return objectNode;
+	}
+
+	/**
+	 * 发送消息
+	 * 
+	 * @param targetType
+	 *            消息投递者类型：users 用户, chatgroups 群组
+	 * @param target
+	 *            接收者ID 必须是数组,数组元素为用户ID或者群组ID
+	 * @param msg
+	 *            消息内容
+	 * @param from
+	 *            发送者
+	 * @param ext
+	 *            扩展字段
+	 * 
+	 * @return 请求响应
+	 */
+	public static ObjectNode sendMessages(String targetType, String[] target, ObjectNode msg, String from,
+			ObjectNode ext) {
+
+		ObjectNode objectNode = factory.objectNode();
+
+		ObjectNode dataNode = factory.objectNode();
+
+		// check appKey format
+		if (!JerseyUtils.match("[0-9a-zA-Z]+#[0-9a-zA-Z]+", APPKEY)) {
+			LOGGER.error("Bad format of Appkey: " + APPKEY);
+
+			objectNode.put("message", "Bad format of Appkey");
+
+			return objectNode;
+		}
+
+		// check properties that must be provided
+		if ("users".equals(targetType) || "chatgroups".equals(targetType)) {
+			LOGGER.error("TargetType must be users or chatgroups .");
+
+			objectNode.put("message", "TargetType must be users or chatgroups .");
+
+			return objectNode;
+		}
+
+		try {
+			// 构造消息体
+			dataNode.put("target_type", targetType);
+			dataNode.put("target", target.toString());
+			dataNode.put("msg", msg);
+			dataNode.put("from", from);
+			dataNode.put("ext", ext);
+
+			Credentail credentail = new UsernamePasswordCredentail(Constants.APP_ADMIN_USERNAME,
+					Constants.APP_ADMIN_PASSWORD, Roles.USER_ROLE_APPADMIN);
+
+			objectNode = HTTPClientUtils.sendHTTPRequest(EndPoints.MANAGEMENT_URL, credentail, dataNode,
+					HTTPMethod.METHOD_POST);
+
+			objectNode = (ObjectNode) objectNode.get("data");
+			for (int i = 0; i < target.length; i++) {
+				String resultStr = objectNode.path(target[i]).asText();
+				if ("success".equals(resultStr)) {
+					LOGGER.error(String.format("Message has been send to user[%s] successfully .", target[i]));
+				} else if (!"success".equals(resultStr)) {
+					LOGGER.error(String.format("Message has been send to user[%s] failed .", target[i]));
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return objectNode;
+	}
 }
