ddb519d3bb3a030db50eda0ea5f071b6a3006bd5
Lynch <sunshinewei_lee@126.com>
2014-09-17 15:48:30 +0800

    add mvn dmeo

--- emchat-server-java-mvn/src/main/java/com/easemob/server/example/httpclient/apidemo/EasemobFiles.java
+++ emchat-server-java-mvn/src/main/java/com/easemob/server/example/httpclient/apidemo/EasemobFiles.java
@@ -1,34 +1,34 @@
-package com.easemob.server.example.jersey.apidemo;
+package com.easemob.server.example.httpclient.apidemo;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
-import org.glassfish.jersey.client.JerseyWebTarget;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.easemob.server.example.comm.Constants;
 import com.easemob.server.example.comm.Roles;
-import com.easemob.server.example.jersey.utils.JerseyUtils;
-import com.easemob.server.example.jersey.vo.Credentail;
-import com.easemob.server.example.jersey.vo.EndPoints;
-import com.easemob.server.example.jersey.vo.UsernamePasswordCredentail;
+import com.easemob.server.example.httpclient.utils.HTTPClientUtils;
+import com.easemob.server.example.httpclient.vo.Credentail;
+import com.easemob.server.example.httpclient.vo.EndPoints;
+import com.easemob.server.example.httpclient.vo.UsernamePasswordCredentail;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
- * REST API Demo : 图片语音文件上传、下载 Jersey2.9实现
+ * REST API Demo : 图片语音文件上传、下载 HttpClient4.3实现
  * 
  * Doc URL: http://www.easemob.com/docs/rest/files/
  * 
- * @author Lynch 2014-09-09
- * 
+ * @author Lynch 2014-09-15
+ *
  */
-class EasemobFiles {
-
+public class EasemobFiles {
 	private static Logger LOGGER = LoggerFactory.getLogger(EasemobFiles.class);
 
 	private static final String APPKEY = Constants.APPKEY;
@@ -57,7 +57,7 @@ class EasemobFiles {
 			return objectNode;
 		}
 
-		if (!JerseyUtils.match("[0-9a-zA-Z]+#[0-9a-zA-Z]+", APPKEY)) {
+		if (!HTTPClientUtils.match("[0-9a-zA-Z]+#[0-9a-zA-Z]+", APPKEY)) {
 			LOGGER.error("Bad format of Appkey: " + APPKEY);
 
 			objectNode.put("message", "Bad format of Appkey");
@@ -70,14 +70,10 @@ class EasemobFiles {
 			Credentail credentail = new UsernamePasswordCredentail(Constants.APP_ADMIN_USERNAME,
 					Constants.APP_ADMIN_PASSWORD, Roles.USER_ROLE_APPADMIN);
 
-			JerseyWebTarget webTarget = null;
-			webTarget = EndPoints.CHATFILES_TARGET.resolveTemplate("org_name", APPKEY.split("#")[0]).resolveTemplate(
-					"app_name", APPKEY.split("#")[1]);
-
 			List<NameValuePair> headers = new ArrayList<NameValuePair>();
 			headers.add(new BasicNameValuePair("restrict-access", "true"));
 
-			objectNode = JerseyUtils.uploadFile(webTarget, uploadFile, credentail, headers);
+			objectNode = HTTPClientUtils.uploadFile(EndPoints.CHATFILES_URL, uploadFile, credentail, headers);
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -106,7 +102,7 @@ class EasemobFiles {
 
 		File downLoadedFile = null;
 
-		if (!JerseyUtils.match("[0-9a-zA-Z-_]+#[0-9a-zA-Z-_]+", APPKEY)) {
+		if (!HTTPClientUtils.match("[0-9a-zA-Z-_]+#[0-9a-zA-Z-_]+", APPKEY)) {
 			LOGGER.error("Bad format of Appkey: " + APPKEY);
 
 			objectNode.put("message", "Bad format of Appkey");
@@ -119,18 +115,18 @@ class EasemobFiles {
 			Credentail credentail = new UsernamePasswordCredentail(Constants.APP_ADMIN_USERNAME,
 					Constants.APP_ADMIN_PASSWORD, Roles.USER_ROLE_APPADMIN);
 
-			JerseyWebTarget webTarget = null;
-			webTarget = EndPoints.CHATFILES_TARGET.resolveTemplate("org_name", APPKEY.split("#")[0])
-					.resolveTemplate("app_name", APPKEY.split("#")[1]).path(fileUUID);
-
 			List<NameValuePair> headers = new ArrayList<NameValuePair>();
-			headers.add(new BasicNameValuePair("share-secret", shareSecret));
+			if (!StringUtils.isEmpty(shareSecret)) {
+				headers.add(new BasicNameValuePair("share-secret", shareSecret));
+			}
 			headers.add(new BasicNameValuePair("Accept", "application/octet-stream"));
 			if (isThumbnail) {
 				headers.add(new BasicNameValuePair("thumbnail", "true"));
 			}
 
-			downLoadedFile = JerseyUtils.downLoadFile(webTarget, credentail, headers, localPath);
+			URL mediaDownloadUrl = HTTPClientUtils
+					.getURL(Constants.APPKEY.replace("#", "/") + "/chatfiles/" + fileUUID);
+			downLoadedFile = HTTPClientUtils.downLoadFile(mediaDownloadUrl, credentail, headers, localPath);
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -144,11 +140,19 @@ class EasemobFiles {
 	}
 
 	public static void main(String[] args) {
-		String fileUUID = "2a6752c0-3e14-11e4-aebc-b1ed5d2b37da";
-		String shareSecret = "KmdSyj4UEeS4QukxV8AP7_gqgxm7lrij5JkdiM06kf6LlFcF";
-		File localPath = new File("/home/lynch/ddd.aa");
-		boolean isThumbnail = false;
-		mediaDownload(fileUUID, shareSecret, localPath, isThumbnail);
-	}
+		File uploadFile = new File("/home/lynch/Pictures/916135926.png");
+		// mediaUpload(uploadFile);
 
+		/**
+		 * { "action" : "post", "application" : "2962b340-0a3b-11e4-b21b-d3b66dbe207b", "params" : { }, "path" :
+		 * "/chatfiles", "uri" : "https://a1.easemob.com/belo/chatapp/chatfiles", "entities" : [ { "uuid" :
+		 * "db1c8dd0-3da4-11e4-8698-b71c9a7d7d34", "type" : "chatfile", "share-secret" :
+		 * "2xy04D2kEeSK5Y1QS-hzJjCR_YvxVlXBXTJqJJSxHYbCdvtu" } ], "timestamp" : 1410873898797, "duration" : 109,
+		 * "organization" : "belo", "applicationName" : "chatapp" }
+		 */
+		String fileUUID = "a9218f70-3da3-11e4-a601-63425f17dd60";
+		File localPath = new File("/home/lynch/Pictures/1111.png");
+		String shareSecret = "2xy04D2kEeSK5Y1QS-hzJjCR_YvxVlXBXTJqJJSxHYbCdvtu";
+		mediaDownload(fileUUID, shareSecret, localPath, false);
+	}
 }
