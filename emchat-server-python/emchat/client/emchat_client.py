7c6c7923ae47a3cc94332ea75b9644ca4dd230d2
Lynch Lee <lynch.lee9527@gmail.com>
2016-11-14 23:35:39 +0800

    support api calls by python

---
+++ emchat-server-python/emchat/client/emchat_client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'Lynch Lee'
+
+
+from emchat.utils.types import *
+from emchat.services.emchat_service import *
+from emchat.utils.confs import *
+
+
+def get_instance(service_type):
+    if service_type == service_users:
+        return EMIMUsersService(org, app)
+    if service_type == service_chatfiles:
+        return EMChatFilesService(org, app)
+    if service_type == service_messages:
+        return EMMessagesService(org, app)
+
+
---
+++ emchat-server-python/emchat/entities/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'Lynch Lee'
