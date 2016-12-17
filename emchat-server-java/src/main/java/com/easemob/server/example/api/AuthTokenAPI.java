6ac2b9d3dc523bcd734798dea168e282ac9a71a1
Jerry Wu <jerrywu12@gmail.com>
2016-12-16 22:43:53 -0800

    add comment for getAuthToken

--- emchat-server-java/src/main/java/com/easemob/server/example/api/AuthTokenAPI.java
+++ emchat-server-java/src/main/java/com/easemob/server/example/api/AuthTokenAPI.java
@@ -1,5 +1,16 @@
 package com.easemob.server.example.api;
 
-public interface AuthTokenAPI{	
+public interface AuthTokenAPI {	
+
+	/**
+	 * Request an Authentication Token <br>
+	 * POST
+	 *
+	 * @param clientId
+	 *            can be found in the application details page of the Hyphenate console
+	 * @param clientSecret
+	 *            can be found in the application details page of the Hyphenate console
+	 * @return
+	 */
 	Object getAuthToken(String clientId, String clientSecret);
 }
