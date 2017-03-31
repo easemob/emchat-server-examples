4aed5ac85c4c1068462ef400448d92064b61d3ee
shanjingheng <shanjh@easemob.com>
2017-03-31 17:23:47 +0800

    release version 1.0.0

--- emchat-server-java/src/main/java/com/easemob/server/example/api/ChatMessageAPI.java
+++ emchat-server-java/src/main/java/com/easemob/server/example/api/ChatMessageAPI.java
@@ -19,6 +19,8 @@ public interface ChatMessageAPI {
 	 * @param query
 	 *            查询语句 <code>ql=select * where timestamp>1403164734226</code>
 	 * @return
+	 *
+	 * 此接口已经过期，下个版本将会提供新接口
 	 */
 	Object exportChatMessages(Long limit, String cursor, String query);
 }
